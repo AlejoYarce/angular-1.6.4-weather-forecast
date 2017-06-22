ed823a931f46df10cca1a63d561c7204dd165d78
Alejo Yarce <alejo.yarce990@gmail.com>
2017-06-22 09:11:00 -0500

    Better UX

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,16 @@
 weatherApp.controller('mainController', [
     '$scope',
+    '$location',
     'forecastService',
-    function ($scope, forecastService) {
+    function ($scope, $location, forecastService) {
         $scope.city = '';
 
         $scope.$watch('city', function () {
             forecastService.city = $scope.city;
         });
+
+        $scope.getForecast = function() {
+            $location.path('/forecast');
+        }
     }
 ]);
